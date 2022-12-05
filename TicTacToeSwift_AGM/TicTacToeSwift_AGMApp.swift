9c56f372feebdd57f8180651e89f695105bc0d6a
jackal14 <agm19a@acu.edu>
2022-12-05 14:04:07 -0600

    Initial Commit

---
+++ TicTacToeSwift_AGM/TicTacToeSwift_AGMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeSwift_AGMApp.swift
+//  TicTacToeSwift_AGM
+//
+//  Created by Anthony Moncivais on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeSwift_AGMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
